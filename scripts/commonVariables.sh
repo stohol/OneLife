9e38d4f75679d17c7a2bbac44e2c92789c34a5f3
connorhsm <connorhsmith@gmail.com>
2020-06-23 22:59:26 +1000

    Create commonVariables.sh for easily managing static values.

---
+++ scripts/commonVariables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PUB_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC42sj/gxduiI4ZJGS+0/zKnuYRY4Q8/5NKU7GJNmyl/sVJ+3anepr7wzz1pZ8sKX6q97RYr4MbstG30cR0SyUX7bBEs403xjJYMmzDfqK7+cbF1FBDjIWiKsiivjAr0kY8NMDKNS6wBXLkSU9SFIyyk+0D0b8/3drVd4i8Jc4mLnan+w3Ed/ncydT6HkP+fzlCqUKJ4gO0DGeHSRdhEi7uw50vCjtObFRf2DRyRFgm0vP3k0pqbb7ldfYy/XRUGGx4WhZVSBUmecfDcVGBJkbTN1+/35IYHGul+2VK3Wmn0ZREHPhHcVejii5pqWDiAcJP3YvjzeZq8zeCWjbLeQG/yJFAx6DMEHYMmawmTpmaaqr+nBUq4EKH5TzK+8Wfer62/4AZSMBzTSHbPVudBc6YscwAX+aL0F1kMuGxuyveNnutVOe+QuyzmgDUoRitq7paUjyCr3fQxY6huhNPEvtA9t5K4mkzBNa6bpYiZ4sY2tqr0rn6HsG4/ZXEfrCWeiPZaZqBToxWk0z6K+zmuDV+z7RypZ9BoPQoUXiI1XARy44iis+4cyAvU8NlKdW/9Hhl/f5FZHwtOy8RY50HV7XE2pfBc61OeW5ksh91oCqPbW7CNPSwKVToGPsY3RT1sYPBEqdrt5ggiDqZDUgog4+pMJ92W+kM5qG/M2SRSBOudQ== connorhsmith@gmail.com"
+
+MAIN_SERVER_IP=64.227.105.24
+
+GAME_SERVER_IP=161.35.225.72
+
+USERNAME="onelife"
+
+DATA_REPO="https://github.com/stohol/OneLifeData7.git"
+
+CODE_REPO="https://github.com/stohol/OneLife.git"
+
+GEMS_REPO="https://github.com/stohol/minorGems.git"
+
+APP_NAME="OneLife"
+
+TAG_BASE="${APP_NAME}_v"
